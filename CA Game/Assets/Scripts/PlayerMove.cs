df5696c65f12195da747f9584be84b66975e3980
Kyle Robert van Winkoop <kylevanwinkoop@gmail.com>
2021-10-16 15:04:55 -0700

    Initial Commit

---
+++ CA Game/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMove : MonoBehaviour
+{
+    public CharacterController2D characterControl;
+
+    public float playerSpeed;
+    
+    private float horizontalMove;
+    private bool jump;
+
+    private bool crouch;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * playerSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetAxisRaw("Vertical") == -1f)
+        {
+            crouch = true;
+        }
+        else
+        {
+            crouch = false;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        characterControl.Move(horizontalMove, crouch, jump);
+        jump = false;
+    }
+}
