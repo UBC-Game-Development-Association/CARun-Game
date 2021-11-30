db5944c340887f57df8d978674133ae296128246
Azura03 <92651705+Azura03@users.noreply.github.com>
2021-11-29 18:46:58 -0800

    Trap Trigger
    Script for detecting when the player clashes with an obstacle.
    Includes a remove health component to cause damage on player health.

---
+++ CA Game/Assets/Scripts/TrapTrigger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Creates a new 2D collider when it is applied
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class TrapTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public int damage;
+    private PlayerHealth health;
+
+    void Start(){
+
+        health = player.GetComponent<PlayerHealth>();
+
+    }
+
+    private void Reset()
+    {
+        GetComponent<BoxCollider2D>().isTrigger=true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            health.removeHealth(damage);
+            Debug.Log(health.healthVal);
+        }
+    }
+}
